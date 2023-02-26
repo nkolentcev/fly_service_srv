f7638d4189a7b8e607a9c017d62a80cb3b2cc7ad
Nikolay Kolentcev <NKolentcev@gmail.com>
2023-02-26 14:56:11 +0300

    add model migration and search user on pin

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/nkolentcev/fly_services_srv/config"
+	"github.com/nkolentcev/fly_services_srv/internal/controllers"
+)
+
+func init() {
+	cfg, err := config.LoadConfig("./../")
+	if err != nil {
+		log.Fatalln("unable to load app.env")
+		log.Fatalln(err.Error())
+	}
+	config.ConnectDB(&cfg)
+}
+
+func main() {
+	app := fiber.New()
+	micro := fiber.New()
+
+	app.Mount("/api", micro)
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PATCH, DELETE",
+		AllowCredentials: true,
+	}))
+
+	micro.Route("/users/:userPin", func(router fiber.Router) {
+		router.Get("", controllers.FindUserByPin)
+	})
+
+	log.Fatal(app.Listen(":8000"))
 }
